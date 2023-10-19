879defeebe46dc74741a7c6cc36d6493808d8587
grelade <jacekgrela@gmail.com>
2023-10-19 13:35:59 +0200

    model update

---
+++ find_good_models.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+python find_good_model.py --model_type knn
+python find_good_model.py --model_type svc
+python find_good_model.py --model_type lgbm
--- prepare_data.py
+++ prepare_data.py
@@ -3,20 +3,19 @@ from glob import glob
 import os
 import re
 import pickle
+import pandas as pd
 
-dir = '../../OASIS-1/'
+dir = 'OASIS-1/'
 dir = Path(dir)
 
 pattern = str(dir)+'/OAS1_*_MR*/PROCESSED/MPRAGE/T88_111/OAS1_*_MR*_mpr_n*_anon_111_t88_masked_gfc_MNI152_T1_1mm.nii.gz'
 files = glob(pattern)
 
-print(len(files))
-
 from mfmri.mfmri.mfmri_lite import LiteMFractalMRI
 
 mfmri = LiteMFractalMRI()
 
-DATA_FILE = 'hurst_data.pickle'
+DATA_FILE = 'temp.pickle'
 if not os.path.exists(DATA_FILE):
     data = {}
 else:
@@ -33,4 +32,11 @@ for i,file in enumerate(files):
         print(f'{i}/{len(files)}: {file_id} exists')
     with open(DATA_FILE,'wb') as fp:
         pickle.dump(data,fp)
-        
+
+# prepare data_hursts.csv
+df = pd.DataFrame(data=data).T.sort_index()
+df = df.loc[:,(~df.isna()).all()]
+mapper = {k:f'slice_{k}' for k in range(df.columns.min(),df.columns.max()+1)}
+df = df.rename(mapper,axis=1)
+
+df.to_csv('data_hursts.csv')
